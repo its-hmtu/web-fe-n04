3b336ce21f9e935cff000de5a53ebb702a0940f2
Hoang Minh Tu <tu.hoangminh15@gmail.com>
2023-10-09 00:01:34 +0700

    update rewards

--- assets/js/btn_slider.js
+++ assets/js/btn_slider.js
@@ -1,32 +1,50 @@
 // @ Hoang Minh Tu - 21011638
+
+// $(document).ready(() => {
+//     const buttons = $('.item-button');
+//     const slider = $('.button-slider');
+//     const thumbnails = $('.thumb-nail-container').find('.thumb-nail-row');
+
+//     var currentButton = buttons[0];
+//     var currentThumbnail = thumbnails[0];
+//     currentThumbnail = $(currentThumbnail);
+//     currentThumbnail.removeClass('thumb-nail-invisible');
+//     currentThumbnail.addClass('thumb-nail-visible');
+
+//     buttons.click(function() {
+//         currentButton = $(this);
+//         var buttonPosition = currentButton.position().left; // Calculate relative position
+//         slider.css('left', buttonPosition + 'px');
+        
+//         slider.css('width', currentButton.outerWidth() + 'px')
+
+//         currentThumbnail.removeClass('thumb-nail-visible');
+//         currentThumbnail.addClass('thumb-nail-invisible');
+
+//         currentThumbnail = $(thumbnails[currentButton.index()]);
+//         currentThumbnail.removeClass('thumb-nail-invisible');
+//         currentThumbnail.addClass('thumb-nail-visible');
+//     });
+// });
+
 $(document).ready(() => {
     const buttons = $('.item-button');
     const slider = $('.button-slider');
     const thumbnails = $('.thumb-nail-container').find('.thumb-nail-row');
-
-
-    var currentButton = buttons[0];
-    var currentThumbnail = thumbnails[0];
-    currentThumbnail = $(currentThumbnail);
-    currentThumbnail.removeClass('thumb-nail-invisible');
-    currentThumbnail.addClass('thumb-nail-visible');
-
-    currentButton = $(currentButton);
-    slider.css('left', currentButton.offset().left + 'px');
+    let currentButton = buttons.first();
+    let currentThumbnail = thumbnails.first().removeClass('thumb-nail-invisible').addClass('thumb-nail-visible');
+  
     buttons.click(function() {
-        currentButton = $(this);
-        slider.css('left', currentButton.offset().left + 'px');
-    
-        // var totalPadding = parseInt(currentButton.css('padding-left')) + 
-        //            parseInt(currentButton.css('padding-right'));
-
-        // slider.css('width', (currentButton.width() + totalPadding) + 'px' );
-
-        currentThumbnail.removeClass('thumb-nail-visible');
-        currentThumbnail.addClass('thumb-nail-invisible');
-
-        currentThumbnail = $(thumbnails[currentButton.index()]);
-        currentThumbnail.removeClass('thumb-nail-invisible');
-        currentThumbnail.addClass('thumb-nail-visible');
+      currentButton = $(this);
+      const buttonPosition = currentButton.position().left;
+      slider.css({
+        left: buttonPosition + 'px',
+        width: currentButton.outerWidth() + 'px'
+      });
+  
+      currentThumbnail.removeClass('thumb-nail-visible').addClass('thumb-nail-invisible');
+      currentThumbnail = $(thumbnails[currentButton.index()]).removeClass('thumb-nail-invisible').addClass('thumb-nail-visible');
     });
-});
+  });
+
+
